77637f773efb2ead652383d9547ef0514025e269
kim12661 <142298745+kim12661@users.noreply.github.com>
2025-04-04 12:32:50 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  // strcspn 함수 사용을 위한 헤더
+
+int main(void) {
+
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \". L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+	return 0;
+}
